9f974359dba4ae1e04958453f1a9ec7a47b395c2
ramya <ramya.pekatanoju@gmail.com>
2024-09-18 17:27:37 +0530

    adding content

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ if [ $USERID  -ne 0 ]
 then 
 
     echo "please run this script with root access."
-
+    exit 1 # manually exit if error comes
 else 
 
     echo "you are super user."
 
 fi
 
-
-
 dnf install mysql -y
+
+echo "is script proceeding?"
