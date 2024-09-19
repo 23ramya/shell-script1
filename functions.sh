b3bfc7c9fe3bab23c8f6ff673c77e856b4149a63
ramya <ramya.pekatanoju@gmail.com>
2024-09-19 21:39:01 +0530

    adding logs

---
+++ functions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+
+if [ $1  -ne 0 ]
+
+then 
+
+    echo "$2...FAILURE"
+    exit 1 
+ else 
+
+    echo "$2....SUCESS "
+
+ fi
+} 
+
+if [ $USERID  -ne 0 ]
+
+then 
+
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes
+else 
+
+    echo "you are super user."
+
+fi
+
+
+dnf install Mysql -y 
+VALIDATE $? "Installing Mysql"
+
+dnf install Git -y 
+VALIDATE $? "Installing Git"
