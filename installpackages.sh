1d9afd2bec9f657b86dcfe50873b3e5cac696c8d
ramya <ramya.pekatanoju@gmail.com>
2024-09-20 10:44:03 +0530

    adding packages

--- installpackages.sh
+++ installpackages.sh
@@ -2,6 +2,16 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
 if [ $USERID  -ne 0 ]
 
 then 
@@ -14,4 +24,11 @@ fi
 for i in $@
 do
    echo "package to install: $i"
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0]
+   then 
+       echo "$i already installed....SKIPPING"
+    else
+        echo "$i Not installed...... Need to install"   
+    fi
 done       
