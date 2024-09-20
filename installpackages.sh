abc3afd778701667ea6e54667771fa24ff936ee1
ramya <ramya.pekatanoju@gmail.com>
2024-09-20 10:53:37 +0530

    adding packages

--- installpackages.sh
+++ installpackages.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -21,14 +29,16 @@ else
     echo "you are super user."
 
 fi
+
 for i in $@
 do
    echo "package to install: $i"
    dnf list installed $i &>>$LOGFILE
    if [ $? -eq 0]
    then 
-       echo "$i already installed....SKIPPING"
+       echo -e "$i already installed....$Y SKIPPING $N"
     else
-        echo "$i Not installed...... Need to install"   
+        dnf install $i -y  &>>$LOGFILE 
+        VALIDATE $? "Installation of $i"
     fi
 done       
